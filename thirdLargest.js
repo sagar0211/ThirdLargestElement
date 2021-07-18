21f5a2d00991b1b93346b936b952d7cdfe86c39f
unknown <sagarparihar002@gmail.com>
2021-07-18 22:09:17 +0530

    variable assign modified

--- thirdLargest.js
+++ thirdLargest.js
@@ -1,30 +1,28 @@
-let arr = [10,50,70,90,10,-5,90,50,40,60,50,90];
-let firstLargestElement = arr[0];
-let secondLargestElement = arr[0];
-let thirdLargestElement = arr[0];
-let newArr =[];
+let arr = [10,50,70,90,10,-5,90,50,40,60,50,90,-80];
+// let arr =[90,20,10]
+let firstLargestElement = Math.min(...arr);
+let secondLargestElement = Math.min(...arr);
+let thirdLargestElement = Math.min(...arr);
+let newArr = [];
 let i;
 
-function thirdLargest()
-{
-  for ( i = 0;i < arr.length ; i++)
-    if (arr[i] > firstLargestElement)
-        firstLargestElement = arr[i];
-        newArr.push(firstLargestElement)
+function thirdLargest() {
+  for (i = 0; i < arr.length; i++)
+    if (arr[i] > firstLargestElement) 
+    firstLargestElement = arr[i];
+  newArr.push(firstLargestElement);
 
-  
-  for ( i = 0;i < arr.length ; i++)
+  for (i = 0; i < arr.length; i++)
     if (arr[i] > secondLargestElement && arr[i] < firstLargestElement)
-        secondLargestElement = arr[i];
-        newArr.push(secondLargestElement)
-  
-  for ( i = 0;i < arr.length ; i++)
+      secondLargestElement = arr[i];
+  newArr.push(secondLargestElement);
+
+  for (i = 0; i < arr.length; i++)
     if (arr[i] > thirdLargestElement && arr[i] < secondLargestElement)
-        thirdLargestElement = arr[i];
-        newArr.push(thirdLargestElement)
-  
+      thirdLargestElement = arr[i];
+  newArr.push(thirdLargestElement);
 }
 thirdLargest();
-console.log("Given Array = ",arr)
-console.log("Three largest element from given array = ",newArr)
-console.log("Third largest element from given array = ",thirdLargestElement)
+console.log("Given Array = ", arr);
+console.log("Three largest element from given array = ", newArr);
+console.log("Third largest element from given array = ", thirdLargestElement);
